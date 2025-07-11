ed4cca3ea313a93671cb812fdc5c53899f8a7d02
benjamin <bs478@sussex.ac.uk>
2025-07-11 14:20:58 +0200

    Began re-designing the LIDARCloud class by adding a LIDARColumn class

---
+++ Utilities/Headers/LIDARColumn.h
@@ -0,0 +1,16 @@
+//
+// Created by benja on 11.07.2025.
+//
+
+#ifndef LIDARCOLUMN_H
+#define LIDARCOLUMN_H
+
+namespace DSLib {
+
+class LIDARColumn {
+
+};
+
+} // DSLib
+
+#endif //LIDARCOLUMN_H
---
+++ Utilities/Implementations/LIDARColumn.cpp
@@ -0,0 +1,8 @@
+//
+// Created by benja on 11.07.2025.
+//
+
+#include "../Headers/LIDARColumn.h"
+
+namespace DSLib {
+} // DSLib
